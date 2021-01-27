145faa2b6b6f0bde557d1091d3ec387de65fb093
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2021-01-27 12:11:26 +0700

    basic SQL exercise from py4e

---
+++ basic SQL/org.py
@@ -0,0 +1,9 @@
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1].split('@')
+    org = email[1]
